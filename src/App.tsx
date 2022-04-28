ddb95872b1d879df0649cd9088df251efe998226
Paulo Henrique <phmelomorais@gmail.com>
2022-04-28 20:12:32 -0300

    Aula 04 - Instalando Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  font-size:64px;
+  color: #b257e6;
+`
+
 export function App() {
   return (
     <div className="App">
-      <h1>hELLO!</h1>
+      <Title>Hello!</Title>
+      <button>Salvar</button>
     </div>
   );
 };
