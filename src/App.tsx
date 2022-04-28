da5379bdd23ec80cfd1a510658c7d648222d9bd6
Paulo Henrique <phmelomorais@gmail.com>
2022-04-28 20:50:17 -0300

    Aula 05 - Criando Estilos Globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  font-size:64px;
-  color: #b257e6;
-`
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <div className="App">
-      <Title>Hello!</Title>
-      <button>Salvar</button>
+      <h1>Hello!</h1>
+      <GlobalStyle/>
     </div>
   );
 };
