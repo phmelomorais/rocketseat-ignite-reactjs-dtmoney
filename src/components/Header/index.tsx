e582853146e4c136c59748dce5ff54d216adf90e
Paulo Henrique <phmelomorais@gmail.com>
2022-04-30 19:47:56 -0300

    Aula 4.01 - Configurando Modal de Criação

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,16 @@
 import logoImg from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
-export function Header() {
-    return (
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void; //É uma função onde o retorno é void.
+}
+
+export function Header({onOpenNewTransactionModal} : HeaderProps) {
+     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="Logo DT Money"/>
-                <button type='button'>
+                <button type='button' onClick={onOpenNewTransactionModal}>
                     Nova transação
                 </button>
             </Content>
