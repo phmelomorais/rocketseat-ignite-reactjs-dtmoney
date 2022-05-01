9439a42894b0cbd76db247881dcca0773c1448a1
Paulo Henrique <phmelomorais@gmail.com>
2022-04-30 21:45:50 -0300

    Aula 4.06 - Funcionamento dos botões

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,15 +1,18 @@
 import Modal from 'react-modal';
-import { Container, TransactionTypeContainer } from './styles';
+import { Container, TransactionTypeContainer, RadioBox } from './styles';
 
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
+import { useState } from 'react';
 interface NewTransactionModalProps {
     isOpen: boolean;
     onRequestClose: () => void;
 }
 
 export function NewTransactionModal( {isOpen, onRequestClose}: NewTransactionModalProps){
+    const [type, setType] = useState('deposit');
+
     return(
         <Modal
             isOpen={isOpen}
@@ -32,14 +35,22 @@ export function NewTransactionModal( {isOpen, onRequestClose}: NewTransactionMod
             />
 
             <TransactionTypeContainer>
-                <button type='button'>
+                <RadioBox 
+                    type='button' 
+                    onClick={() => setType('deposit')}
+                    isActive={type === 'deposit'}
+                >
                     <img src={incomeImg} alt="Entrada" />
                     <span>Entrada</span>
-                </button>
-                <button type='button'>
+                </RadioBox>
+                <RadioBox 
+                    type='button' 
+                    onClick={() => setType('withdraw')}
+                    isActive={type === 'withdraw'}
+                >
                     <img src={outcomeImg} alt="Saída" />
                     <span>Saída</span>
-                </button>
+                </RadioBox>
 
             </TransactionTypeContainer>
 
