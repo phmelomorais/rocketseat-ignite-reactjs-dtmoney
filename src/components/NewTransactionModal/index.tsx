e630d2c0bc654f608a1d62fb8b0457d03949a406
Paulo Henrique <phmelomorais@gmail.com>
2022-04-30 21:17:57 -0300

    Aula 4.05 - Criando botões de tipo

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,9 @@
 import Modal from 'react-modal';
-import { Container } from './styles';
+import { Container, TransactionTypeContainer } from './styles';
 
 import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
 interface NewTransactionModalProps {
     isOpen: boolean;
     onRequestClose: () => void;
@@ -28,6 +30,19 @@ export function NewTransactionModal( {isOpen, onRequestClose}: NewTransactionMod
                 placeholder='Valor'
                 type="number"
             />
+
+            <TransactionTypeContainer>
+                <button type='button'>
+                    <img src={incomeImg} alt="Entrada" />
+                    <span>Entrada</span>
+                </button>
+                <button type='button'>
+                    <img src={outcomeImg} alt="Saída" />
+                    <span>Saída</span>
+                </button>
+
+            </TransactionTypeContainer>
+
             <input 
                 placeholder='Categoria' 
             />
